7e0ec3d4e2a21eae7b92888efffb654cd55bc75e
daliaemad11 <daliaemad671@gmail.com>
2024-08-08 23:25:41 +0300

    profile_card

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Card from './componants/Card/Card'
+
+
+function App() {
+
+  return (
+    <>
+     <main>
+     <Card></Card>
+      
+     </main>
+    </>
+  )
+}
+
+export default App
